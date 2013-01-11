acb15ec8c7825e6f95cf0b531cefd9eb378b887f
jreyes01 <itsjustreyes@gmail.com>
2013-01-11 14:35:23 -0800

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,19 @@ class User < ActiveRecord::Base
   				  :first_name, :last_name, :profile_name 
   # attr_accessible :title, :body
 
+validates :first_name,presence:true 
+
+validates :last_name,presence:true
+
+validates :profile_name,presence:true,
+                        uniqueness:true,
+                        format:{
+                          with: /[a-zA-Z0-9_-]+/,
+                          message:'Must be formatted correctly.'
+                        }
+
+
+
   has_many :statuses
 
   def full_name 
@@ -20,7 +33,7 @@ class User < ActiveRecord::Base
 
     def gravatar_url
       stripped_email = email.strip
-      downcase_email = stripped_email.downcase
+      downcased_email = stripped_email.downcase
       hash = Digest::MD5.hexdigest(downcase_email)
 
       "http://gravatar.com/avatar/#{hash}"
