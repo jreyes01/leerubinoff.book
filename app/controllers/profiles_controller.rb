e331071d75191fd3c8ac9446c3492433d54b64fe
jreyes01 <itsjustreyes@gmail.com>
2013-01-11 10:47:47 -0800

    Fixed syntax and other shit.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
   		@statuses = @user.statuses.all
   		render action: :show
   	else
-  	render file: 'public/404' status: 404, formats:[:html]
+  	render file: 'public/404',status: 404,formats:[:html]
   end
-  end
+end
 end
